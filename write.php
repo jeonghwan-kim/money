c99283fd21f1b3b3d096aef1c1957e0f431169bf
jeonghwan <ej88ej@gmail.com>
2012-11-29 21:19:34 +0900

    삭제 기능 추가

--- write.php
+++ write.php
@@ -3,17 +3,27 @@
 $filename = $_REQUEST["xml_filename"];
 
 if (file_exists($filename)) {
-	$data = file_get_contents($filename);
-	echo $data;
+	$data = file_get_contents($filename);	
+	// echo 'asdf';
 } else {
 	$data = "<?xml version=\"1.0\" encoding=\"euc-kr\"?><expense></expense>";
-	echo $data;
 }
 
 $xml = new simpleXmlElement($data);
 
+// 마지막 entry id 구하기
+$end_id = -1;
+foreach ($xml->entry as $entry) {
+	if ((int)($entry->id) > $end_id)
+		$end_id = $entry->id;
+}
+
+// 새로운 entry id 구하기
+$new_id = $end_id + 1;
+
 // xml에 데이터 추가하기
 $new_entry = $xml->addChild("entry");
+$new_entry->addChild("id", $new_id);
 $new_entry->addChild("date", $_REQUEST["date"]);
 $new_entry->addChild("item", $_REQUEST["item"]);
 $new_entry->addChild("text", $_REQUEST["text"]);
