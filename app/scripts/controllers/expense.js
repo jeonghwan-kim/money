38552032a130cc9fc5f653811e0d986620fbb9a0
jeonghwan <ej88ej@gmail.com>
2014-04-27 11:34:27 +0900

    편집기능 추가: 다음번 작업은 코드 단순화 할것. 1.  지출이력 받아오기 / 월 목록 받아오기 api를 각각 분리할 것.

--- app/scripts/controllers/expense.js
+++ app/scripts/controllers/expense.js
@@ -3,33 +3,31 @@
 angular.module('moneyApp')
   .controller('ExpenseCtrl', function ($scope, $http, $routeParams, $route, $location, $cookies) {
 
+    // 지출 이력을 받아온다.
     $http.get('/api/expense/' + $routeParams.yearMonth)
       .success(function(data) {
         $scope.expense = data.data;
         $scope.sum = data.total;
         $scope.curMonth = data.curMonth;
         $scope.yearmonth = data.monthList;
+
+        console.log(data.data);
       })
       .error(function(data, status) {
         $location.url('/login');
       });
     ;
 
-    $scope.deleteExpense = function(expenseId) {
-
-      console.log(expenseId);
+    $scope.deleteExpense = function() {
+      var delIdx = $scope.editExpenseId;
+      var delExpenseId;
 
       if (confirm('삭제할까요?', true)) {
-        var len = $scope.expense.length;
-        for (var i = 0; i < len; i++) {
-          if ($scope.expense[i].id === expenseId) {
-            $scope.expense.splice(i, 1);
-            break;
-          }
-        }
+        delExpenseId = $scope.expense[delIdx].id;
+        $scope.expense.splice(delIdx, 1);
 
         // 서버 삭제 비동기 처리
-        $http.delete('/api/expense/' + expenseId);
+        $http.delete('/api/expense/' + delExpenseId);
       } else {
         return false;
       }
@@ -45,23 +43,29 @@ angular.module('moneyApp')
       });
     }
 
-    $scope.updateModal = function(date, text, amount, expenseId) {
-      console.log(date, text, amount, expenseId);
-
-      $scope.modal = {};
-      $scope.modal.date = date.split(/T/)[0];
-      $scope.modal.text = text;
-      $scope.modal.amount = amount;
-      $scope.modal.expenseId = expenseId;
+    $scope.updateModal = function(expenseId) {
+      var len = $scope.expense.length;
+      for (var i = 0; i < len; i++) {
+        if ($scope.expense[i].id == expenseId) {
+          $scope.editExpenseId = i;
+          // console.log('searched!', new Date($scope.expense[i].date));
+          return;
+        }
+      }
     };
 
     $scope.updateExpense = function() {
-      var url = '/api/expense/' + $scope.modal.expenseId;
+      var url = '/api/expense/' + $scope.expense[$scope.editExpenseId].id;
       var data = {
-        date: $scope.modal.date,
-        text: $scope.modal.text,
-        amount: $scope.modal.amount
+        date: $scope.expense[$scope.editExpenseId].date,
+        text: $scope.expense[$scope.editExpenseId].text,
+        amount: $scope.expense[$scope.editExpenseId].amount
       };
+
+      console.log('edited url: ' + url);
+      console.log('edited data: ' + data);
+
+      // return;
 
       $http.put(url, data)
         .success(function(data, status, headers, config) {
