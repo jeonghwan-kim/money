9cb19935a534809f5571630f73982d5f0ad98193
jeonghwan <ej88ej@gmail.com>
2015-05-01 22:39:36 +0900

    Install angular-fullstack

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/money-dev'
+  },
+
+  seedDB: true
+};
