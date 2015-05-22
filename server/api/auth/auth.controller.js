4f5313156c78b655da1309c79fa931aeb20a5de2
jeonghwan <ej88ej@gmail.com>
2015-05-22 21:45:44 +0900

    #44, #48

--- server/api/auth/auth.controller.js
+++ server/api/auth/auth.controller.js
@@ -1,19 +1,47 @@
 'use strict';
 
 var _ = require('lodash');
+var models = require('../../models');
+var email = require('../../components/email');
 
 // Get auth info saved in session memory
-exports.index = function(req, res) {
+exports.index = function (req, res) {
   res.json(req.user);
 };
 
 // Login
-exports.login = function(req, res) {
+exports.login = function (req, res) {
   res.status(201).json(req.user);
 };
 
 // Logout
-exports.logout = function(req, res) {
+exports.logout = function (req, res) {
   req.logout();
   res.json({logout: true});
+};
+
+// Reset password
+exports.resetPassword = function (req, res) {
+  var newPassword = new Date().getTime().toString().substring(8, 12);
+  var newPassword2 = require('crypto').createHash('md5').update(newPassword).digest('hex');
+
+  models.User.findOne({where: {email: req.body.email}}).then(function (user) {
+    if (!user) {
+      res.status(404).send();
+    }
+
+    user.updateAttributes({pass: newPassword2}).then(function (affectedCount) {
+      if (affectedCount) {
+        email('Reset Password: ' + newPassword, '', user.email).then(function (info) {
+          console.log(info);
+          res.json(info);
+        }, function (error) {
+          console.error(error);
+          res.status(500).json(error);
+        })
+      } else {
+        res.status(404).send();
+      }
+    })
+  })
 };
--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -4,11 +4,23 @@ var express = require('express');
 var passport = require('passport');
 var controller = require('./auth.controller');
 var ensureAuth = require('./ensure-auth');
-
+var Joi = require('express-joi').Joi;
+var joiValidate = require('express-joi').joiValidate;
 var router = express.Router();
+
+var schema = {
+  find: {
+  },
+  update: {
+    email: Joi.types.String().required()
+  },
+  remove: {
+  }
+};
 
 router.get('/', ensureAuth, controller.index);
 router.post('/', passport.authenticate('local'), controller.login);
 router.delete('/', ensureAuth, controller.logout);
+router.put('/', joiValidate(schema.update, {strict: true}), controller.resetPassword);
 
 module.exports = router;
